3bb8c59ba4689e441438e2922c324728300afb15
Andrei Rozhkov <polax2008@gmail.com>
2017-11-13 22:01:23 +0100

    Test saving in the file notes.json

--- notes/notes.js
+++ notes/notes.js
@@ -1,11 +1,16 @@
+const fs = require('fs');
+
 module.exports = {
 	
 	addNote(title, body) {
+		const note = { title, body };
+		fs.writeFileSync('notes.json', JSON.stringify(note));
 		console.log('Adding note: ', title, body);
 	},
 
 	getList() {
-		console.log('Gettong list of notes');
+		const note = fs.readFileSync('notes.json');
+		console.log('Getting list of notes', JSON.parse(note));
 	},
 
 	readNote(title) {
@@ -15,5 +20,5 @@ module.exports = {
 	removeNote(title) {
 		console.log('Removing note: ', title);
 	}
-	
+
 };
